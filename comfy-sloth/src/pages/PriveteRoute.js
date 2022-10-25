142e6a8cf986d60f4ecbc688fde51a20de828dd9
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-10-25 13:26:24 +0500

    C/*[C

---
+++ comfy-sloth/src/pages/PriveteRoute.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PriveteRoute = () => {
+  return (
+    <div>PriveteRoute</div>
+  )
+}
+
+export default PriveteRoute
---
+++ comfy-sloth/src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from "./HomePage";
+import Products from "./ProductPage";
+import SingleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import PriveteRoute from "./PriveteRoute";
+import AuthWrapper from "./AuthWrapper";
+
+export {
+    Home,
+    Products,
+    SingleProduct,
+    About,
+    Cart,
+    Error,
+    Checkout,
+    PriveteRoute,
+    AuthWrapper
+}
